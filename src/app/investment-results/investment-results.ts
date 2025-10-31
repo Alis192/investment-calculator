b9661781b7673a5666e41f6797ddb08e64da04cf
Ali Damirov <alidov363@gmail.com>
2025-10-31 07:04:32 +0100

    Changed logic to use signals

--- src/app/investment-results/investment-results.ts
+++ src/app/investment-results/investment-results.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -12,7 +12,6 @@ import { InvestmentService } from '../investment.service';
 export class InvestmentResults {
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultData;
-  }
+  // Using computed to reactively get the result data signal
+  results = computed(() => this.investmentService.resultData());
 }
