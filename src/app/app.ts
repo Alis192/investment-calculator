72557f72265bdcfbc0a39e623fafb255c72926b0
Ali Damirov <alidov363@gmail.com>
2025-10-31 06:55:23 +0100

    Added a service to handle calculation logic for components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { UserInput } from "./user-input/user-input";
-import type { InvestmentInput } from './investment-input.model';
 import { InvestmentResults } from "./investment-results/investment-results";
 
 @Component({
@@ -11,37 +10,5 @@ import { InvestmentResults } from "./investment-results/investment-results";
   styleUrl: './app.css'
 })
 export class App {
-  resultsData = signal<{
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[] | undefined>(undefined);
 
-
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData.set(annualData);
-  }
 }
